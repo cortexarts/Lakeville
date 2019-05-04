4cdf2d03d07cdfaf3d8eb785eee54db836a89b5a
Jasper Drescher <jasperdre@gmail.com>
2019-05-04 14:17:53 +0200

    Change offset and lookat method.

--- Lakeville/Assets/Scripts/ThirdPersonCameraController.cs
+++ Lakeville/Assets/Scripts/ThirdPersonCameraController.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class ThirdPersonCameraController : MonoBehaviour
 {
-    public GameObject player;
-
+    public Transform target;
     private Vector3 offset;
 
-    // Use this for initialization
-    void Start()
+    private void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        offset = transform.position - target.position;
     }
 
-    // LateUpdate is called after Update each frame
-    void LateUpdate()
+    private void Update()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
-        transform.rotation = player.transform.rotation;
+        transform.position = target.position + offset;
+        Camera.main.transform.LookAt(target);
     }
 }
